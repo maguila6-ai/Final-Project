d9a599d1b916b893fa87bbd9d4432f29ac5d5588
maguila6 <maguila6@charlotte.edu>
2026-04-14 20:32:26 -0400

    Create model_loader.py

---
+++ FinalProject/api/schemas/model_loader.py
@@ -0,0 +1,6 @@
+from app.models.user import User
+from app.models.assignment import Assignment
+from app.models.course import Course
+from app.models.group import Group
+from app.models.task import Task
+from app.models.notification import Notification
